340332f93b503209376dd0bd9f3f0cfa94486bff
Souvik Mukherjee <mukherjeesouvik606@gmail.com>
2024-12-02 02:25:55 +0530

    pair-game

--- src/components/Pair Game/GameBoard.jsx
+++ src/components/Pair Game/GameBoard.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Card from "./Card";
 
-function GameBoard({ cards, flippedCards, matchedCards, onCardClick,difficulty}) {
+function GameBoard({ cards, flippedCards, matchedCards, onCardClick, difficulty }) {
   const gridColumns = difficulty === "easy" ? 4 : difficulty === "medium" ? 6 : 8;
   console.log(gridColumns);
   return (
-    <div className={`grid ${gridColumns===4?'grid-cols-4':gridColumns===6?'grid-cols-6':'grid-cols-8'} gap-4 p-4`}>
-      {cards.map((card, index) => (
-        <Card
-          key={card.id}
-          value={card.value}
-          isFlipped={flippedCards.includes(index) || matchedCards.includes(index)}
-          onClick={() => onCardClick(index)}
-        />
-      ))}
+    <div className="flex justify-center items-center`">
+      <div className={`border-4 border-purple-700 rounded-2xl grid ${gridColumns === 4 ? 'grid-cols-4' : gridColumns === 6 ? 'grid-cols-6' : 'grid-cols-8'} gap-4 p-4`}>
+        {cards.map((card, index) => (
+          <Card
+            key={card.id}
+            value={card.value}
+            isFlipped={flippedCards.includes(index) || matchedCards.includes(index)}
+            onClick={() => onCardClick(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
